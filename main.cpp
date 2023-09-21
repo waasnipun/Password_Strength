9b7a5934341c3f40b55210875467627f18c0e7e2
waasnipun <waasnipun@gmail.com>
2023-09-21 21:19:07 +0300

    Update: Changing build system to bazel

--- main.cpp
+++ main.cpp
@@ -1,17 +1,9 @@
 // C++ program to check if a given password is
 // strong or not.
 #include <iostream>
-#include "src/passchecker.h"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {   
-    string input;
-    cout<<"Enter your password:\n";
-    cin>>input;
-	 
-	string results = passchecker(input);
-	cout << "Strength of password: " << results << endl;;
-	return 0;
 }
